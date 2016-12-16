edce0dc61920b34b1b1eecd1396b66686bbcfa84
Daniyar Itegulov <ditegulov@gmail.com>
2016-12-16 18:01:28 +0300

    Implement BencodeParser

--- src/main/java/org/jtorrent/client/bencode/BencodeParser.java
+++ src/main/java/org/jtorrent/client/bencode/BencodeParser.java
@@ -1,7 +1,15 @@
 package org.jtorrent.client.bencode;
 
+import org.jtorrent.client.util.PeekableAsciiInputStream;
+
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class BencodeParser {
     private static final BencodeParser INSTANCE = new BencodeParser();
@@ -14,6 +22,40 @@ public class BencodeParser {
     }
 
     public BObject parse(InputStream is) throws IOException {
-        return null;
+        PeekableAsciiInputStream asciiInputStream = new PeekableAsciiInputStream(is);
+        return parse(asciiInputStream);
+    }
+
+    private BObject parse(PeekableAsciiInputStream asciiInputStream) throws IOException {
+        char c = (char) asciiInputStream.peek();
+        switch (c) {
+            case 'd':
+                assertThat(asciiInputStream.read() == 'd');
+                Map<BObject, BObject> map = new HashMap<>();
+                while (asciiInputStream.peek() != 'e') {
+                    BObject key = parse(asciiInputStream);
+                    BObject value = parse(asciiInputStream);
+                    map.put(key, value);
+                }
+                assertThat(asciiInputStream.read() == 'e');
+                return new BDictionary(map);
+            case 'l':
+                assertThat(asciiInputStream.read() == 'l');
+                List<BObject> list = new ArrayList<>();
+                while (asciiInputStream.peek() != 'e') {
+                    list.add(parse(asciiInputStream));
+                }
+                assertThat(asciiInputStream.read() == 'e');
+                return new BList(list);
+            case 'i':
+                assertThat(asciiInputStream.read() == 'i');
+                BObject result = new BLong(asciiInputStream.readLong());
+                assertThat(asciiInputStream.read() == 'e');
+                return result;
+            default:
+                long length = asciiInputStream.readLong();
+                assertThat(asciiInputStream.read() == ':');
+                return new BString(asciiInputStream.readString(length));
+        }
     }
 }
